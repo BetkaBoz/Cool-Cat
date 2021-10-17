23a3481a187cc17f0081bb8d6b3f02a5f9ca78d8
BetkaBoz <132betka231@gmail.com>
2021-10-17 21:06:14 +0200

    Laravel db setup

---
+++ cool-cat-db/app/Http/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class HomeController extends Controller
+{
+    public function test(){
+        echo "GET READY COOL CAT";
+    }
+}
