27a5c6f018b9d3086352e639f286755d5fe3490f
MatusValko <mvmatusvalko@gmail.com>
2021-10-27 22:49:57 +0200

    created user moddel

--- cool-cat-db/app/Models/User.php
+++ cool-cat-db/app/Models/User.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+    
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
 }
