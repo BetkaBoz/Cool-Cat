67955783901def02e9b20333ec27b3298b7276f8
BetkaBoz <132betka231@gmail.com>
2021-11-29 11:19:39 +0100

    Applying Login UI and base web styling

--- cat-face/src/views/auth/Login.vue
+++ cat-face/src/views/auth/Login.vue
@@ -1,35 +1,38 @@
 <template>
   <div>
+    <br/><br/><br/>
     <div class="row justify-content-center">
-      <div class="col-md-6">
+      <h2>Start your cooking adventure!</h2>
+      <div class="col-md-4">
+        <br/>
         <div class="card">
-          <div class="card-header">Login</div>
           <div class="card-body">
-            <div class="form-group">
-              <label for="email">Email</label>
-              <input v-model="formData.email" type="text" class="form-control" name="email">
-              <p class="table-danger" v-text="errors.email"></p>
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input v-model="formData.password" type="password" class="form-control" name="password">
-              <p class="table-danger" v-text="errors.password"></p>
-            </div>
+            <br/>
+            <form>
 
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <button @click="login" class="btn btn-primary">Login</button>
-                </div>
+              <div class="form-group">
+                <input v-model="formData.email" type="text" class="form-control" name="email" placeholder="Email">
+                <p class="table-danger" v-text="errors.email"></p>
               </div>
-              <div class="col-md-6 text-right">
-                <router-link to="/register">Create New Account</router-link>
+
+              <div class="form-group">
+                <input v-model="formData.password" type="password" class="form-control" name="password" placeholder="Password">
+                <p class="table-danger" v-text="errors.password"></p>
               </div>
+
+              <div class="form-group">
+                <button @click="login" class="btn btn-primary">Login</button>
+              </div>
+            </form>
+            <br/>
+            <div class="col-md-12 text-center">
+              <p>Haven't joined yet? Register <router-link to="/register">here</router-link>!</p>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <br/><br/><br/>
   </div>
 </template>
 
@@ -65,5 +68,25 @@ export default {
 </script>
 
 <style scoped>
-
+.card {
+  background-color: rgba(96,111,123,0)
+}
+h2 {
+  color: rgba(255, 255, 255, 0.85);
+}
+button {
+  background-color: #0dc6fd61;
+  border-color: #0dc6fd61;
+}
+button:hover {
+  background-color: rgba(13, 198, 253, 0.75);
+  border-color: rgba(13, 198, 253, 0.75);
+}
+a {
+  color: rgba(13, 198, 253, 0.55);
+  text-decoration: none;
+}
+a:hover {
+  color: rgba(13, 198, 253, 0.75);
+}
 </style>
